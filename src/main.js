c5ff12f5fe4755909f76131a6d03a7ed2b734caf
Zhijie Zhang <zhijiezhang@ZhijiedeMacBook-Pro.local>
2019-07-10 00:19:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './routers';
+import store from './store';
+import 'amfe-flexible'
+import axios from 'axios'
+Vue.prototype.$axios = axios;
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
