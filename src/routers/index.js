17538713b74e78fb8a749d0412390de0ecba2477
chaowei <804549193@qq.com>
2019-07-11 21:08:54 +0800

    再次修改固定底部导航，将主页分离出App.vue

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,13 +3,17 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
-
+import Home from '../pages/Home.vue';
 import Discover from '../pages/Discover.vue';
 import Production from '../pages/Production.vue';
 import Mine from '../pages/Mine.vue';
 
 let router = new VueRouter({
     routes: [{
+        name: 'Home',
+        path: '/home',
+        component: Home,
+    }, {
         name: 'Production',
         path: '/production',
         component: Production,
