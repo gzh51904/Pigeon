ff3096b2584e8649eac58051c4f0c3aa19be0939
yangyuning <yangyuning@qq.com>
2019-07-11 14:48:40 +0800

    发现中的（推荐）静态页面

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <sub-nav></sub-nav>
   </div>
 </template>
 
 <script>
 import "../static/css/reset.css";
+
+import subNav from "./components/Subnav.vue";
+
 export default {
   name: "app",
-  components: {}
+  data: {},
+  components: {
+    subNav
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log(to, from);
+    next();
+  },
+  created() {
+    console.log(1);
+  }
 };
 </script>
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -17,32 +17,6 @@ let router = new VueRouter({
         name: 'Discover',
         path: '/discover',
         component: Discover,
-        // 嵌套路由
-        // children: [
-        //     {
-        //         name: 'Recommend',
-        //         path: 'recommend',
-        //         component: Recommend
-        //     }, {
-        //         name: 'Features',
-        //         path: 'reatures',
-        //         component: Features
-        //     }, {
-        //         name: 'Basis',
-        //         path: 'basis',
-        //         component: Basis
-        //     }, {
-        //         name: 'Insurance',
-        //         path: 'insurance',
-        //         component: Insurance
-        //     }, {
-        //         name: 'Claims',
-        //         path: 'claims',
-        //         component: Claims
-        //     }
-        // ]
-
-
     }, {
         name: 'Mine',
         path: '/mine',
