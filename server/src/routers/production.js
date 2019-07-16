a271a8eb87db3645961b03297efa3f0e1b0d94f3
Zhijie Zhang <zhijiezhang@ZhijiedeMacBook-Pro.local>
2019-07-17 01:20:34 +0800

    node服务器

---
+++ server/src/routers/production.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const Router = express.Router();
+
+const {
+    mongo: {
+        find
+    }
+} = require('../db');
+const {
+    formatData
+} = require('../utils')
+
+Router.get('/', async (req, res) => {
+    let {
+        type
+    } = req.query;
+    console.log(type);
+    if(!type){
+        var result = await find('productions', {}, {});
+    }
+    else{
+        var result = await find('productions', {type}, {});
+     }
+    console.log(result);
+    res.send(formatData(
+        result
+    ));
+
+})
+
+
+module.exports = Router;
